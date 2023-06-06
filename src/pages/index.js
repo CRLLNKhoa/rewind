c5d12c96fd24657ca925fe57ec9b6c14771ee5cf
Lương Nguyễn Khoa <107914230+CRLLNKhoa@users.noreply.github.com>
2023-06-06 10:08:11 +0700

    Analyt

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { data } from "./api/hello";
 import { time } from "./api/time";
 import { useState } from "react";
 import Link from "next/link";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -47,10 +46,6 @@ export default function Home() {
   }
   return (
     <div className="flex justify-center items-center bg-slate-950 w-fulll min-h-screen">
-     <Head>
-        <title>Code by Lương Khoa</title>
-        <meta name="Check rewind" content="Website này tôi làm ccho vui, nếu bạn có không thích thì tôi thật lòng xin lỗi bạn  vì điều này! " key="desc" />
-      </Head>
       {tab===1&&<div className="border w-full lg:w-[80%] min-h-[80%] p-4 text-white flex justify-center items-center flex-col gap-2">
         <h1 className="text-[20px]">Mốc Rewind Nhanh</h1>
         <p className="pb-2">Cost càng nhỏ thời gian rewind càng nhanh</p>
